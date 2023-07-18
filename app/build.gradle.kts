65ba06e449074bae272bb41644fa93cd69f246a0
Alexander Georgievskiy <galeksandrp@gmail.com>
2023-07-18 11:25:01 +0300

    Update android.yml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
     buildTypes {
         debug {
             applicationIdSuffix = ".debug"
+            signingConfig = signing
         }
         release {
             isMinifyEnabled = true
